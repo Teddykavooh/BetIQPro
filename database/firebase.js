db5259b3c7666f30b1732ba774d1db1e53d4eff4
Teddykavooh <teddykavooh@gmail.com>
2023-10-15 19:58:44 +0300

    project initialization

---
+++ database/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDKGLgYGDXgUlkv-BnZFrU_Jspj8wYwCMI",
+  authDomain: "betiqpro.firebaseapp.com",
+  projectId: "betiqpro",
+  storageBucket: "betiqpro.appspot.com",
+  messagingSenderId: "583792942825",
+  appId: "1:583792942825:web:e589c41c4191e11c9a5a78",
+  measurementId: "G-EN562LYCDQ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
