c7902e4cc6db3914086f64587377887bd5a49784
Teddykavooh <teddykavooh@gmail.com>
2023-10-29 16:17:35 +0300

    Complete app HeaderLogo

--- components/header.js
+++ components/header.js
@@ -1,15 +1,29 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 const HeaderLogo = () => {
   return (
-    <Text style={styles.logoText}>
-      BET<span style={styles.whiteText}>IQ</span>PRO
-    </Text>
+    <View style={styles.logoContainer}>
+      <Image
+        style={styles.logoIcon}
+        source={require("../assets/logo_icon.png")}
+      />
+      <Text style={styles.logoText}>
+        BET<Text style={styles.whiteText}>IQ</Text>PRO
+      </Text>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  logoContainer: {
+    flexDirection: "row", // Arrange the image and text horizontally
+    alignItems: "center", // Center them vertically
+  },
+  logoIcon: {
+    width: 30, // Adjust the width as needed
+    height: 35, // Adjust the height as needed
+  },
   logoText: {
     // Define any common styles for the logo text here.
     fontStyle: "italic",
