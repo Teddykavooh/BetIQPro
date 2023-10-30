141d16e7b2d76200a905a09f29abf848a11d7ab3
Teddykavooh <teddykavooh@gmail.com>
2023-10-30 13:29:36 +0300

    Add tableview component

--- components/history.js
+++ components/history.js
@@ -2,15 +2,42 @@ import * as React from "react";
 import { Text, View, StyleSheet } from "react-native";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 
+function TableView() {
+  return (
+    <View style={styles.tableContainer}>
+      <View style={styles.tableColumn1}>
+        <Text>Time</Text>
+      </View>
+      <View style={styles.tableColumn2}>
+        <Text>League</Text>
+        <View style={styles.textLayout1}>
+          <Text>Home</Text>
+          <Text>Away</Text>
+        </View>
+        <View style={styles.textLayout1}>
+          <Text style={styles.cl2_oddLabel}>ODD Label</Text>
+          <Text style={styles.cl2_odd}>ODDs</Text>
+        </View>
+      </View>
+      <View style={styles.tableColumn3}>
+        <Text style={styles.cl3_odd}>Status</Text>
+        <Text style={styles.cl3_odd}>Score</Text>
+      </View>
+    </View>
+  );
+}
+
 function FreeTips() {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text style={styles.headerText}>Header Section</Text>
+        <Text style={styles.headerTextRounded}>Today</Text>
+        <Text style={styles.headerTextRounded}>Date</Text>
       </View>
 
       <View style={styles.content}>
         <Text style={styles.contentText}>Content Section</Text>
+        <TableView />
       </View>
     </View>
   );
@@ -83,10 +110,19 @@ const styles = StyleSheet.create({
   header: {
     backgroundColor: "white",
     padding: 10,
+    flexDirection: "row",
   },
   headerText: {
     fontSize: 20,
     fontWeight: "bold",
+  },
+  headerTextRounded: {
+    backgroundColor: "#E7DFEC", // Background color
+    borderRadius: 15, // Border radius
+    fontSize: 17,
+    color: "#554D5A",
+    padding: 5,
+    marginRight: 10,
   },
   content: {
     flex: 1,
@@ -96,5 +132,70 @@ const styles = StyleSheet.create({
   contentText: {
     fontSize: 16,
     color: "white",
+  },
+  tableContainer: {
+    flexDirection: "row",
+    height: 100,
+    padding: 5,
+    marginBottom: 10,
+  },
+  tableColumn1: {
+    backgroundColor: "red",
+    flex: 0.2,
+    borderWidth: 1,
+    borderColor: "white",
+    borderTopStartRadius: 10,
+    borderBottomStartRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  tableColumn2: {
+    backgroundColor: "green",
+    flex: 0.6,
+    borderWidth: 1,
+    borderColor: "white",
+    borderStartWidth: 0,
+    borderEndWidth: 0,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  tableColumn3: {
+    backgroundColor: "blue",
+    flex: 0.2,
+    borderWidth: 1,
+    borderColor: "white",
+    borderTopEndRadius: 10,
+    borderBottomEndRadius: 10,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  textLayout1: {
+    flexDirection: "row",
+    gap: 45,
+  },
+  textLayout2: {
+    flexDirection: "column",
+  },
+  cl2_oddLabel: {
+    color: "black",
+    backgroundColor: "#FFFFFF",
+    borderWidth: 1,
+    borderColor: "white",
+    borderRadius: 6,
+  },
+  cl2_odd: {
+    color: "#4C0400",
+    backgroundColor: "#F88E10",
+    borderWidth: 1,
+    borderColor: "white",
+    borderRadius: 6,
+  },
+  cl3_odd: {
+    color: "#55C147",
+    backgroundColor: "#2B2D27",
+    borderWidth: 1,
+    borderColor: "white",
+    borderRadius: 6,
   },
 });
